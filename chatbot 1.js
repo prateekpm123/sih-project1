1c0eef5f821f95f0478ef3c31d67bb5da629f4cf
prateekpm123 <prateekpm123@gmail.com>
2020-01-13 15:57:03 +0530

    Added multiple button functionality according to the array in the chat bot

--- chatbot 1.js
+++ chatbot 1.js
@@ -1,11 +1,11 @@
 var list = ["What is your complaint about", "what is your specialization",
-    "Which year are you in "
+    "Which year are you in ", "What is your favourite animal"
 ];
 var answer2 = [
     ["sakec", "somaiya", "vjti"],
-    ["it", "cs", "sxtc"],
-    ["1st", "2nd", "3rd"],
-    ['meow','dog', 'cat']
+    ["it", "cs", "sxtc","mech"],
+    ["1st", "2nd", "3rd","iit", "mit"],
+    ['meow','dog']
 ];
 
 let para = document.getElementById('para');
@@ -15,10 +15,10 @@ var count = 0;
 function toCreateNewButton(count) {
 
     var num = answer2[count].length;
-    var test8 = document.getElementById('buttonrows');
-    var newbutton = document.createElement('button');
-    // alert('in the new button')
-    for (var i = 0; i < 3; i++) {
+    // var test8 = document.getElementById('buttonrows');
+    // var newbutton = document.createElement('button');
+    // alert('in the new button'+count+"num = "+num)
+    for (var i = 0; i < num; i++) {
         //   alert('in create button')
         var test8 = document.getElementById('buttonrows');
         var newbutton = document.createElement('button');
@@ -33,10 +33,11 @@ function toCreateNewButton(count) {
 
 function removingExtraButtons(count) {
     // alert('in the remove function')
+    var num = answer2[count-1].length;
     var parent = document.getElementById("buttonrows");
     var child = document.getElementsByClassName("btn1");
     var childn = child.length - 1;
-    for (var n = 0; n < 3; n++) {
+    for (var n = 0; n < num; n++) {
         parent.removeChild(child[0]);
     }
 
